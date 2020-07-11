7ab9cb0c106cd10e528f555ad33dba84b6c77c8e
Timmy Cho <chotimmysf@gmail.com>
2020-07-10 20:00:46 -0700

    removed unnecessary text

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 const container = document.querySelector(".container");
-const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const seats = document.querySelectorAll(
+  ".row .seat:not(.occupied) .seat:not(.coronavirus)"
+);
 const count = document.getElementById("count");
 const count = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
-const ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row.seat.selected");
 
-  console.log(selectedSeats);
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
 }
+
+// Movie select event
+movieSelect.addEvenetListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+// Seat click event
 container.addEventListener("click", (e) => {
   if (
     e.target.classList.contains("seat") &&
-    !e.target.classList.contains("occupied")
+    !e.target.classList.contains("occupied") &&
+    !e.target.classList.contains("coronavirus")
   ) {
     e.target.classList.toggle("selected");
 
