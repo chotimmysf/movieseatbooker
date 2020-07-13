5d1ec56f0fd0e6f553488bc5f97d372824378e98
Timmy Cho <chotimmysf@gmail.com>
2020-07-12 22:17:33 -0700

    nodelist returning zeroes, revert custom changes

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
 const container = document.querySelector(".container");
-const seats = document.querySelectorAll(
-  ".row .seat:not(.occupied) .seat:not(.coronavirus)"
-);
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
-const count = document.getElementById("total");
+const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
-let ticketPrice = +movieSelect.value;
+const ticketPrice = +movieSelect.value;
 
-// Update total and count
+// Update total and count (NOT WORKING)
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row.seat.selected");
 
   const selectedSeatsCount = selectedSeats.length;
 
+  console.log(selectedSeatsCount);
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
-
-// Movie select event
-movieSelect.addEvenetListener("change", (e) => {
-  ticketPrice = +e.target.value;
-  updateSelectedCount();
-});
 
 // Seat click event
 container.addEventListener("click", (e) => {
   if (
     e.target.classList.contains("seat") &&
-    !e.target.classList.contains("occupied") &&
-    !e.target.classList.contains("coronavirus")
+    !e.target.classList.contains("occupied")
   ) {
     e.target.classList.toggle("selected");
 
